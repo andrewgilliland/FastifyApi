d3e8f4423a4a2eab70a09a61684a42b08f65fb38
Andrew Gilliland <andrewpgilliland@gmail.com>
2024-01-03 15:52:03 -0600

    feat: adds prisma and gets all exercises

--- index.ts
+++ index.ts
@@ -1,9 +1,32 @@
 import fastify from "fastify";
+import { PrismaClient } from "@prisma/client";
 
 const server = fastify();
+const prisma = new PrismaClient();
 
-server.get("/exercises", async (request, reply) => {
-  return "get all exercises\n";
+server.get("/exercises", async (req, res) => {
+  const exercises = await prisma.exercises.findMany();
+
+  console.log("exercises: ", exercises);
+  return exercises;
+});
+
+server.post("/create-exercise", async (req, res) => {
+  //   const exercise = await prisma.exercise.create({
+  //     data: {
+  //       name: "clap pushup",
+  //       difficulty: "HARD",
+  //       equipment: "NONE",
+  //       exerciseType: "STRENGTH",
+  //       forceType: "PUSH",
+  //       mechanics: "COMPOUND",
+  //       secondaryMuscleGroup: ["triceps"],
+  //       targetMuscleGroup: "chest",
+  //     },
+  //   });
+
+  //   console.log("exercise", exercise);
+  return `create exercise\n`;
 });
 
 server.get("/workouts", async (request, reply) => {
