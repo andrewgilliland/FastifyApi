23d6ffd201e3c07b5317f49f5c1d3ec96356e97b
Andrew Gilliland <andrewpgilliland@gmail.com>
2024-02-25 09:13:15 -0600

    feat: updates server port for docker

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,12 @@
 import server from "./app";
 
-console.log("process.env: ", process.env.NODE_ENV);
-
 const port = process.env.NODE_ENV === "development" ? 8080 : 3000;
 
-console.log("port: ", port);
-
-server.listen({ port }, (err, address) => {
+server.listen({ port, host: "0.0.0.0" }, (err, address) => {
   if (err) {
     console.error(err);
     process.exit(1);
   }
+
   console.log(`Server listening at ${address}`);
 });
